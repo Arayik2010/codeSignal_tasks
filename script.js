3c6be64a4f8c2ed4f101c46716285c12440e0495
Arayik2010 <71314631+Arayik2010@users.noreply.github.com>
2022-06-04 21:42:18 +0400

    task 1

---
+++ script.js
@@ -0,0 +1,12 @@
+///1.Write a function that returns the sum of two numbers.//
+
+//Example//
+
+//For param1 = 1 and param2 = 2, the output should be solution(param1, param2) = 3//
+
+function solution(param1, param2) {
+  let sum = 0;
+  sum = param1 + param2;
+  return sum;
+}
+console.log(solution(1,2))
