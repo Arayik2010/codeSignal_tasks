135dd2cdb8a797904c40daf80acb2ec930717250
Arayik2010 <71314631+Arayik2010@users.noreply.github.com>
2022-06-27 00:48:10 +0400

    exercises-10

--- script.js
+++ script.js
@@ -150,6 +150,22 @@
 //  let result = arr1.filter((item) => item.length === arrEndElement.length);
 //  return result;
 
-
 // }
 // console.log(solution(["abc", "eeee", "abcd", "dcd"]));
+
+//10.Given two strings, find the number of common characters between them.
+//Example
+//For s1 = "aabcc" and s2 = "adcaa", the output should be
+//solution(s1, s2) = 3.
+
+// function solution(s1, s2) {
+//  let count = 0;
+//     for(let i = 0; i < s1.length; i++){
+//         if(s2.indexOf(s1[i]) !=-1){
+//             count++;
+//             s2 = s2.replace(s1[i], '');
+//         }
+//     }
+//     return count;
+//   }
+// console.log(solution("aabcc", "adcaa"));
